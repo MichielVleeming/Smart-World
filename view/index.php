58b55e8c33e8d9bdc60859118a33d8d5a9fbe86d
Barturiot <barturiot500@hotmail.nl>
2017-11-01 09:25:24 +0100

    css toegevoegd en login verwijderen

---
+++ view/index.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+	</head>
+
+	<body>
+
+		<ul>
+  			<li><a href="#home">Home</a></li>
+  			<li><a href="#news">News</a></li>
+  			<li><a href="#contact">Contact</a></li>
+  			<li style="float:right"><a class="active" href="#about">About</a></li>
+		</ul>
+
+	</body>
+</html>
