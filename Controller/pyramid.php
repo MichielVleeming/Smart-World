4aa8c72f6cd9c789a1f6444a6b5e84ff46a19a56
MichielVleeming <Michiel.vleeming@hotmail.com>
2017-11-01 12:05:30 +0100

    Added MVC Model + pyramid w/ gray as standard.

---
+++ Controller/pyramid.php
@@ -0,0 +1,19 @@
+<?php
+include("../Model/dbase.php");
+// Set array values to use later
+
+
+
+$result = $sql->query("SELECT * FROM dataset2015");
+for ($set = array (); $row = $result->fetch_assoc(); $set[] = $row);
+
+$numbers = array ();
+$SVG_paths = array ();
+for ($x = 0; $x < count($set); $x++){
+    $numbers[$x] = $set[$x]["Percentage"];
+    $SVG_paths[$x] = $set[$x]["Pcodering"];
+}
+echo $numbers[2];
+$sql->close();
+
+?>
