3b485920ef39ab202d5299a71bb178c248d32cde
Barturiot <barturiot500@hotmail.nl>
2017-11-07 14:37:01 +0100

    Include map aangemaakt en daarin header.php gemaakt. in header.php zit de nav. Deze kan in anderen files geincluded worden wat nu ook gedaan wordt in index.php

--- view/index.php
+++ view/index.php
@@ -1,16 +1,13 @@
 <html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
-	</head>
+    <head>
+        <link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+        <?php
+            include '../includes/header.php';
+        ?>
+    </head>
 
-	<body>
-
-		<ul>
-  			<li><a href="#home">Home</a></li>
-  			<li><a href="#news">News</a></li>
-  			<li><a href="#contact">Contact</a></li>
-  			<li style="float:right"><a class="active" href="#about">About</a></li>
-		</ul>
-
-	</body>
+    <body>
+        <p>Dit is het hoofd scherm van de verkeersveiligheidsmonitor nadat je bent ingelogd als een gebruiker</p>
+        
+    </body>
 </html>
