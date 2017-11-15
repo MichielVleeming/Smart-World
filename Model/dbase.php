985c85ea92e803956ba5208b8c614191e79afa08
Barturiot <barturiot500@hotmail.nl>
2017-11-15 10:53:08 +0100

    Nieuwe database connectie toegevoegd in comment. Database connectie is nu in OOP gemaakt

--- Model/dbase.php
+++ Model/dbase.php
@@ -12,3 +12,37 @@ $sql = new MySQLi($host, $username, $password, $dbname);
 
 
 ?>
+<!--
+<?php
+class MySQLConnection {
+
+    private $dbhost = 'localhost';                                
+    private $dbuser = 'root';
+    private $dbpassword = '123';
+    private $dbname = 'verkeersveiligheidmonitor';
+
+    private $connection;  
+    
+    public function MySQLConnection() {
+            $this->openConnection();           
+    }
+
+    public function openConnection() {
+        $this->connection = 
+                mysqli_connect($this->dbhost, 
+                        $this->dbuser, 
+                        $this->dbpassword, 
+                        $this->dbname);
+        if (!$this->connection) {
+            die("Database connection error: " . mysqli_error($this->connection));
+        	} 
+        return $this->connection;      
+    }        
+
+	public function query($sql) {
+        $this->last_query = $sql;
+        $result = mysqli_query($this->connection, $sql);
+        return $result;
+    }     
+
+} -->
