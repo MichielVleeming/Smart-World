075e44c96701edc2e4fbd8035f7d492aace6d5bc
Barturiot <barturiot500@hotmail.nl>
2018-01-10 11:47:30 +0100

    alles bij elkaar gezet

--- includes/footer.php
+++ includes/footer.php
@@ -5,20 +5,20 @@
 <footer class="footer">
     <ul class="menuleft">
         <h2>Kom in contact</h2>
-        <li><span>Email: info@RHDHV.com</span></li>
-        <li><span>Serversdesk: 036 4857362</span></li>
+        <li><span><a href="mailto:help@RHDHV.com">Email Ons</a> </span></li>
+        <li><span>Serversdesk: 036-4857362</span></li>
     </ul>
 
     <ul class="menucenter">
         <h2>Wie en waarom?</h2>
-        <li><a href="index.php">RHDHV</a></li>
-        <li><a href="index.php">ROV</a></li>
+        <li><a href="https://www.royalhaskoningdhv.com/">RHDHV</a></li>
+        <li><a href="http://www.rovoostnederland.nl/">ROV</a></li>
     </ul>
 
     <ul class="menuright">
         <h2>Blijf op de hoogte</h2>
-        <li><a href="index.php">Facebook</a></li>
-        <li><a href="index.php">Nieuwsbrief</a></li>
+        <li><a href="https://www.facebook.com/RoyalHaskoningDHV/">Facebook</a></li>
+        <li><a href="">Nieuwsbrief</a></li>
     </ul>
 
     <img src="http://www.nlingenieurs.nl/wp-content/uploads/2013/03/logo-Royal-HaskoningDHV.jpg">
--- includes/header.php
+++ includes/header.php
@@ -7,54 +7,39 @@
 
         <nav class="navbar">
             <ul>
-                <li><a href="index.php" style="text-decoration:none; color:black;">Home</a></li>
+                <li><a href="index.php" style="text-decoration:none; color:white;">Home</a></li>
                 <li>Rapportages
                 <ul class="dropdown">
-                    <li>Piramides
+                    <li>Piramide
                     <ul class="submenu">
-                        <li><a href="monitor.php" style="color:black;">Piramide 2016</a></li>
-                        <li><a href="monitor.php" style="color:black;">Piramide 2015</a></li>
-                        <li><a href="monitor.php" style="color:black;">Piramide 2014</a></li>
+                        <li><a href="monitor.php" style="color:white;">Piramide 2016</a></li>
+                        <li><a href="monitor.php" style="color:white;">Piramide 2015</a></li>
+                        <li><a href="monitor.php" style="color:white;">Piramide 2014</a></li>
                     </ul>
                     </li>
-                <li>Tussenrapportage
+                <li>Dashboard
                 <ul class="submenu">
-                    <li>Test</li>
-                    <li>Test</li>
-                    <li>Test</li>
-                    <li>Test</li>
                 </ul>
                 </li>
-                <li><a href="enquete.php" style="color:black;">Enquette</a></li>
-                <li>Test</li>
-                <li>Test</li>
+                <li><a href="enquete.php" style="color:white;">Enquette</a></li>
             </ul>
             </li>
             <li>&nbsp;&nbsp;&nbsp;Analyse &nbsp;&nbsp;&nbsp;
             <ul class="dropdown">
-                <li><a href="AutoMonitor.php" style="color:black;">Auto/bestelauto</a></li>
-                    <li><a href="Bikemonitor.php" style="color:black;">Fiets/snorfiets</a></li>
-                    <li><a href="Motormonitor.php" style="color:black;">Bromfietsen</a></li>
+                <li><a href="AutoMonitor.php" style="color:white;">Auto/bestelauto</a></li>
+                    <li><a href="Bikemonitor.php" style="color:white;">Fiets/snorfiets</a></li>
+                    <li><a href="Motormonitor.php" style="color:white;">Bromfietsen</a></li>
                 </ul>
             </li>
             <li>Organisatie
             <ul class="dropdown">
-                <li>Test</li>
-                <li>Test</li>
-                <li>Test</li>
             </ul>
             </li>
             <li>Informatie
             <ul class="dropdown">
-                <li>Test</li>
-                <li>Test</li>
-                <li>Test</li>
             </ul>
             <li>
-                <form action="../controller/loginController.php" method="post" style="color:black;">
-                    <input type="hidden" name="action" value="logout">
-                    <input type="submit" name="submit" value="Logout">
-                </form>
+            <a href="../controller/logincontroller.php?action=logout&submit=Logout" style="color:white;">Logout</a>
             </li>
             </li>
             </ul>
